e53c03879af36f38c0b72641dd7b777cd4efb2a0
Jon Börjesson <jon@spuun.se>
2016-04-13 09:49:03 +0200

    Removed stuff to start over

--- apiclient.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-	var api = function(method, cb) {
-		$.getJSON(location.protocol+'//api.'+location.hostname.replace(/[^.]*\.?(.+)$/,'$1')+':'+location.port+'/'+method, cb);
-	}
-	api.ere = function(cb) {
-		api('ere', cb);
-	}
-	window.kebabfredag = api;
-})(window.jQuery);
