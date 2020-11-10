ac0e9600e3eb919ef72634e93af880ad2ee4090a
Jon Börjesson <jon@spuun.se>
2020-11-10 12:36:28 +0100

    Remove unused code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const template = require('jontemplate');
 const express = require('express');
 const kebabfredag = require('./kebabfredag');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 app.engine('html', template());
@@ -13,10 +14,9 @@ app.use('/api', require('./api'));
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-
-	res.render('index', {
-		answer: kebabfredag.ere() 
-	});
+  res.render('index', {
+    answer: kebabfredag.ere()
+  });
 });
 
 module.exports = app;
